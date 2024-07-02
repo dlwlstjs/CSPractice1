4565147d3a04ee0c43405b04af1b06cb6ab2ddac
dlwlstjs <dlwlstjs0413@gmail.com>
2024-07-02 14:45:36 +0900

    #5 22-9. 값 복사와 참조 복사

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -71,6 +71,14 @@ namespace CSPractice1
     }
     internal class Program
     {
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test t)
+        {
+            t.value = 20;
+        }
         class ThirdClass
         {
 
@@ -278,6 +286,11 @@ namespace CSPractice1
             b.Height = 100;
             b.Height = -100;
 
+            // #5 22-9. 값 복사와 참조 복사
+            Test test = new Test();
+            test.value = 100;
+            Change(test);
+            Console.WriteLine(test.value); //20
         }
     }
 }
